be49b7f9765f954878d9c1143fb98ec84975343c
Mohamed-Sala77 <mohamedsalah2001412@gmail.com>
2022-05-10 01:20:51 +0200

    add tm4c123 .h

--- main project/main project.c
+++ main project/main project.c
@@ -1,5 +1,6 @@
 #include "Io.h" 
 #include "initialization.h" 
+#include "tm4c123gh6pm2.h" 
 #define NVIC_ST_CTRL_R          (*((volatile unsigned long *)0xE000E010))
 #define NVIC_ST_RELOAD_R        (*((volatile unsigned long *)0xE000E014))
 #define NVIC_ST_CURRENT_R       (*((volatile unsigned long *)0xE000E018))
