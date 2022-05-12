043b59718d5d29a7e827ce7dbe786d9eab06d00c
WalidSalah21 <Salahwalid235@gmail.com>
2022-05-12 16:42:39 +0200

    make function

---
+++ main project/functions.h
@@ -0,0 +1,143 @@
+    #include "Io.h" 
+    #include "tm4c123gh6pm2.h" 
+    #include "stdint.h"
+    #include "string.h"
+     enum CASE {OFF=0 , ON=1};
+    #define NVIC_ST_CTRL_R          (*((volatile unsigned long *)0xE000E010))
+    #define NVIC_ST_RELOAD_R        (*((volatile unsigned long *)0xE000E014))
+    #define NVIC_ST_CURRENT_R       (*((volatile unsigned long *)0xE000E018))
+
+    int check_door (){
+    SW3_INIT ();
+    //GET_SW3
+    while(1){
+    if (SW3==ON)
+    {
+    //TIMER_COUNT
+    return (int)(ON);
+    break; 
+    }
+    else if (SW3==OFF)
+    {
+    //print_str("Close the door for safity plz ♥ \n");
+    return (int)(OFF);
+    }
+
+    }
+    //LED_BLINK
+    //BUZZ
+    }
+    void NUM_SEC(unsigned long num)
+    {
+    int i;
+			
+    for (int i = 0; i < num; i++)
+		{
+			systick_init(0X04C4B400); //ONE SEC
+		}
+	
+		}
+
+	void NUM_MIN(unsigned long num )
+		{
+		int i;
+		int j;
+			for (int j = 0; j < (num-1); i++)
+			{
+			for (int i = 0; i < 60; i++)
+			{
+				systick_init(0X04C4B400); //ONE SEC
+			}
+			}
+	
+		}
+
+		void LED_END(){
+		GPIO_PORTF_DATA_R &= ~0x0E;
+        NUM_SEC(1)
+		for(i=0;i < 6;i++){
+		GPIO_PORTF_DATA_R ^= 0x0E;
+		NUM_SEC(1);
+		}
+		}
+
+        void LED_BLINK(){
+		
+		while(1){
+        LED_Init () ;
+		GPIO_PORTF_DATA_R ^= 0x0E;
+		NUM_SEC(1);
+		}
+		}
+
+        void TIMER_D(){
+        int n;
+        uint32_t i;
+        uint32_t j ;
+        uint32_t total_min ;
+        uint32_t total_sec ;
+        
+         i=//GIT_FROM_KYPAD)();
+         //LCD"00:0I"
+         j=i<<8;
+         i=//GIT_FROM_KYPAD)()
+         
+        
+        total_min =(i | j) ;
+        while(1){
+        if(total_min<=30){
+            /*LCD"total_min:00"*/
+            break;
+            }
+        else //printf"PLZ ENTER BELOW 30 MIN"
+        }
+
+        //------------------------------------
+
+        
+         i=//GIT_FROM_KYPAD();
+        //LCD"00:0I"
+         j=i<<8
+         i=//GIT_FROM_KYPAD();
+         
+        total_sec =(i | j) ;
+        //LCD"00:total_sec"
+            /*LCD"00:total_sec"*/
+           
+        }
+        
+        float NUMBER MIN_BEEF KILO(char kilos  /*GIT_FROM_KYPAD)()*/ )
+        {
+        float BEEF_TIME;
+
+        while(1){
+        if (kilos<=9){
+            BEEF_TIME=0.5*kilos;
+        return (float) (BEEF_TIME)
+        break;
+        }
+        else
+        {
+         return (char) 10   //اكبر من 4.5 نعمل داله هناك تطلع 0
+        }
+        }
+        
+        }
+
+        float NUMBER MIN_CHICKEN KILO(char kilos  /*GIT_FROM_KYPAD)()*/ )
+        {
+            float CHICKEN_TIME
+      while(1){
+        if (kilos<=9){
+            CHICKEN_TIME=0.2*kilos;
+        return (float) (CHICKEN_TIME)
+        break;
+        }
+        else
+        {
+         return (char) 10   //اكبر من 1.8 نعمل داله هناك تطلع 0
+        }
+        }
+        }
+        
+
